700c7c9df18afb588a6a8f8a1469d1a2d8d2bdcd
akaio <a.arnur181@gmail.com>
2026-02-24 00:45:19 +0500

    ended 4

--- tasks.py
+++ tasks.py
@@ -1,2 +1,10 @@
 def create_task(title):
     print(f"Task '{title}' created")
+
+def list_tasks():
+    print("Listing tasks...")
+
+def delete_task(title):
+    print(f"Task '{title}' deleted")
+
+
